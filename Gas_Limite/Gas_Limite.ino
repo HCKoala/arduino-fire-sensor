785c5312ae86d72baa5a223cd7d6c526e154e656
Yusuf <admin@halaycekenkoala.com>
2017-04-27 09:21:28 +0300

    Update Gas_Limite.ino

--- Gas_Limite/Gas_Limite.ino
+++ Gas_Limite/Gas_Limite.ino
@@ -1,4 +1,4 @@
-//Değişkenler tanımlandı.
+// Değişkenler tanımlandı.
 int sensor=1; // Gaz sensörü arduino'nun analog 1 pinine bağlandı.
 int led1=8; // LED 8. dijital çıkış pinine bağlandı.
 int led2=7; // LED 7. dijital çıkış pinine bağlandı.
@@ -14,14 +14,14 @@ void setup() {
   pinMode(led1, OUTPUT); // LED çıkış yapıldı.
   pinMode(led2, OUTPUT); // LED çıkış yapıldı.
   pinMode(buzzer, OUTPUT); // Buzzer çıkış yapıldı.
-  Serial.begin(9600); //Saniyede yapılacak bit transferi miktarı belirlendi.
-  pinMode(sensorPin, INPUT); //Alev sensörü giriş olarak belirlendi.
+  Serial.begin(9600); // Saniyede yapılacak bit transferi miktarı belirlendi.
+  pinMode(sensorPin, INPUT); // Alev sensörü giriş olarak belirlendi.
 }
 
-//Sürekli tekrarlanacak kodlar;
+// Sürekli tekrarlanacak kodlar;
 void loop() {
   gas_limite=analogRead(sensor); // Gaz sensöründen değer okunuyor.
-  smoke_level= digitalRead(sensorPin); //Alev sensöründen değer okunuyor.
+  smoke_level= digitalRead(sensorPin); // Alev sensöründen değer okunuyor.
   Serial.print("Gaz Durumu: "); // Seri haberleşme ekranında Gaz Durumu yazısı yazdırıldı.
   Serial.print(gas_limite); // Seri haberleşme ekranında gaz limiti değeri gösteriliyor.
   Serial.println(" Limit"); // Seri haberleşme ekranında okunan değerin yanına Limit yazdırıldı.
@@ -38,7 +38,7 @@ void loop() {
   Serial.println("Gaz Limiti Normal"); // Seri haberleşme ekranında Gaz Limiti Normal yazısı yazdırıldı.
   digitalWrite(led1, HIGH); // Yeşil LED'e sinyal gönderildi.
   digitalWrite(led2, LOW); // Kırmızı LED'e gönderilen sinyal kesildi.
-  noTone(buzzer); // uzzer'a gönderilen sinyal kesildi.
+  noTone(buzzer); // Buzzer'a gönderilen sinyal kesildi.
   }
 
 }
